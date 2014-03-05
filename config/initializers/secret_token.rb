9d4d4fa9f72d3806797d1213da97377425ab64fa
shanmugavel <iashanmugavel@gmail.com>
2014-03-05 12:55:40 +0530

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Ideologysoft::Application.config.secret_key_base = 'b95511aa2d7f3041462abb3d1de97ebf784f1c136defef0af4cd7424f1b6afb682051f85dd6c243924f53da6a2e529c854aa039a8de515daf58838370e332a13'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Ideologysoft::Application.config.session_store :cookie_store, key: '_ideologysoft_session'
